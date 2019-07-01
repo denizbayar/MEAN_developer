5a26f0a3ff461da572c563bc3f22a79a29b48b23
denizbayar <deniz.bayar@ekinnolab.eu>
2019-07-01 21:59:56 +0200

    Landing page implemented with React.js

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
