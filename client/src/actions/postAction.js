74c431b05741025d065d1b80dc3a37615ab1a8c8
Deniz Bayar <deniz.bayar@ekinnolab.eu>
2020-01-07 21:51:19 +0100

    actions completed

---
+++ client/src/actions/postAction.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import { ADD_POST, GET_POSTS, GET_POST, DELETE_POST, POST_LOADING, GET_ERRORS } from './types';
+
+export const addPost = postData => dispatch => {
+    axios.post('/api/posts', postData).then(res => dispatch({
+        type: ADD_POST,
+        payload: res.data
+    })).catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }))
+}
+
+export const getPosts = () => dispatch => {
+    console.log('Post Called')
+    dispatch(getPostLoading());
+    axios.get('/api/posts').then(res => dispatch({
+        type: GET_POSTS,
+        payload: res.data
+    })).catch(err => dispatch({
+        type: GET_POSTS,
+        payload: null
+    }))
+}
+
+export const getPost = (id) => dispatch => {
+    dispatch(getPostLoading());
+    axios.get(`/api/posts/${id}`).then(res => dispatch({
+        type: GET_POST,
+        payload: res.data
+    })).catch(err => dispatch({
+        type: GET_POST,
+        payload: null
+    }))
+}
+
+export const deletePost = id => dispatch => {
+    axios.delete(`/api/posts/${id}`).then(res => dispatch({
+        type: DELETE_POST,
+        payload: id
+    })).catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }))
+}
+
+export const addLike = id => dispatch => {
+    axios.post(`/api/posts/like/${id}`)
+        .then(res => dispatch(getPost()))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+export const removeLike = id => dispatch => {
+    axios.post(`/api/posts/unlike/${id}`)
+        .then(res => dispatch(getPost()))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+export const addComment = (postId, commentData) => dispatch => {
+    axios.post(`/api/posts/comment/${postId}`, commentData).then(res => dispatch({
+        type: GET_POST,
+        payload: res.data
+    })).catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }))
+}
+export const deleteComment = (postId, commentId) => dispatch => {
+    axios.delete(`/api/posts/comment/${postId}/${commentId}`).then(res => dispatch({
+        type: GET_POST,
+        payload: res.data
+    })).catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }))
+}
+
+const getPostLoading = () => {
+    return {
+        type: POST_LOADING
+    }
+}
---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,144 @@
+import axios from 'axios';
+
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER, GET_PROFILES } from './types';
+
+//Get Current Profile
+export const getCurrentProfile = () => dispatch => {
+    //Use this data for spinner
+    dispatch(setProfileLoading());
+    //Get Profile
+    axios.get('/api/profile')
+        .then(res => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        }).catch(err => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: {}
+            })
+        })
+}
+//Get Profile by Handle
+export const getProfileByHandle = (handle) => dispatch => {
+    //Use this data for spinner
+    dispatch(setProfileLoading());
+    //Get Profile
+    axios.get(`/api/profile/handle/${handle}`)
+        .then(res => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        }).catch(err => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: null
+            })
+        })
+}
+
+
+//Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios.post('/api/profile', profileData).then(
+        res => history.push('/dashboard')).catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+
+}
+//Delete account & profile
+export const deleteAccount = () => dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+        axios
+            .delete('/api/profile')
+            .then(res => dispatch({ type: SET_CURRENT_USER, payload: {} }))
+            .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }))
+    }
+}
+
+//Profile Loading
+export const setProfileLoading = () => {
+    return {
+        type: PROFILE_LOADING
+    }
+}
+
+//Clear Profile
+export const clearCurrentProfile = () => {
+    return {
+        type: CLEAR_CURRENT_PROFILE
+    }
+}
+
+//Get Profiles
+export const getProfiles = () => dispatch => {
+    //Use this data for spinner
+    dispatch(setProfileLoading());
+    axios
+        .get('/api/profile/all')
+        .then(res =>
+            dispatch({
+                type: GET_PROFILES,
+                payload: res.data
+            })
+        )
+        .catch(err => {
+            dispatch({
+                type: GET_PROFILES,
+                payload: null
+            })
+        })
+}
+
+//Add Experience
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post('/api/profile/experience', expData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+//Delete Experience
+export const deleteExperience = (id) => dispatch => {
+    axios
+        .delete(`/api/profile/experience/${id}`)
+        .then(res => dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        }))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+//Add Education
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+        .post('/api/profile/education', eduData)
+        .then(res => history.push('/dashboard'))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
+
+//Delete Education
+export const deleteEducation = (id) => dispatch => {
+    axios
+        .delete(`/api/profile/education/${id}`)
+        .then(res => dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        }))
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }))
+}
