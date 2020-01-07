8bacdc9647f7fe1ab806a363ee5bf9e72ea026a3
Deniz Bayar <deniz.bayar@ekinnolab.eu>
2020-01-07 21:50:10 +0100

    router and components are added

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,82 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken'
 
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import PrivateRoute from './components/common/PrivateRoute';
+
+import { clearCurrentProfile } from './actions/profileActions';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+
+
+
 
 import './App.css';
 
+//check token
+if (localStorage.jwtToken) {
+  //Set isAuthorized true
+  setAuthToken(localStorage.jwtToken);
+  //decode user token and get user info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000;
+  //Check token is expired
+  if (decoded.exp < currentTime) {
+    //Logout User 
+    store.dispatch(logoutUser());
+    //Remove Profile
+    store.dispatch(clearCurrentProfile());
+    //Redirect to login Page
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <div className="container">
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <div className="container">
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:handle' component={Profile} />
+              <Route exact path='/not-found' component={NotFound} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+              <PrivateRoute exact path='/add-experience' component={AddExperience} />
+              <PrivateRoute exact path='/add-education' component={AddEducation} />
+              <PrivateRoute exact path='/feed' component={Posts} />
+              <PrivateRoute exact path='/post/:id' component={Post} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
