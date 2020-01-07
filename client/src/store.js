e6c6b2aa050911b4d0f11304def01b1bce9f3c11
Deniz Bayar <deniz.bayar@ekinnolab.eu>
2020-01-07 21:50:25 +0100

    root reducers added

---
+++ client/src/store.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducers from './reducers' //normally it is './reducers/index' however we don't need to put 'index'.
+
+const middleware = [thunk];
+const initialState = {};
+
+const store = createStore(rootReducers, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+export default store;
